ae64e8626ec6bb22c5575881fc36b0c7335e675f
Rob Montero <rmontero@phase2technology.com>
2020-01-23 13:55:04 -0600

    Update favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -19,6 +19,10 @@ export default function Layout(props) {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Helmet>
       <Header page={props.page} title={title} />
       <div className={layoutStyles.content}>{props.children}</div>
